ab4f8c2fb8a7e72edf2383d16828fc0f4127816a
ricardo94ac <richardo94ac@gmail.com>
2025-02-11 13:58:11 +0100

    4º commit añadir archivo de texto con los resultados

--- holamundo.sh
+++ holamundo.sh
@@ -1,2 +1,4 @@
 echo "Tu pc está siendo escaneado por un virus,pinche aquí para pararlo"
 sudo apt install nmap #le obligamos a instalar nmap para descubrir que puertos tiene vulnerable
+nmap -p 1-5000 127.0.0.1 -oN resultado_nmap.txt #le escaneamos los puertos del 1 al 5000
+#le añadimos el archivo de texto donde mandara los resultados
