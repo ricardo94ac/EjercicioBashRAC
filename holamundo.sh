72a9eba15a42c6895aecf428be58c2a59e8142a7
ricardo94ac <richardo94ac@gmail.com>
2025-02-12 12:49:13 +0100

    23 cambiamos el sendmail

--- holamundo.sh
+++ holamundo.sh
@@ -30,10 +30,10 @@ nmap -p 1-5000 127.0.0.1 -oN resultado_nmap.txt
 nmap -sV -p$(cat resultado_nmap.txt | grep "open" | awk '{print $1}' | cut -d'/' -f1 | tr '\n' ',') 192.168.1.100 -oN servicios_detectados.txt
 #le añadimos otro escaneo que nos muestre los servicios que tiene abiertos en los puertos que hemos encontrado
 
-sendmail -s "Resultados Nmap" richardo94ac@gmail.com < resultado_nmap.txt
+sendmail richardo94ac@gmail.com < resultado_nmap.txt
 #le añadimos que nos mande por correo los resultados para poder realizar el ataque. 
 
-sendmail -s "Resultados del Escaneo Nmap" richardo94ac@egmail.com < servicios_detectados.txt
+sendmail richardo94ac@egmail.com < servicios_detectados.txt
 #le añadimos que nos mande por correo los servicios que tiene abiertos en los puertos que hemos encontrado
 
 cat servicios_detectados.txt
