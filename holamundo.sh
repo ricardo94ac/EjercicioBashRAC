3800b3faa635d628b6bbfeafc36e7611f9ad8048
ricardo94ac <richardo94ac@gmail.com>
2025-02-17 08:55:02 +0100

    26 comentamos todo lo relacionado con mandarlo por email ya que no nos funciona

--- holamundo.sh
+++ holamundo.sh
@@ -1,14 +1,14 @@
  sudo apt install nmap -y
 #le obligamos a instalar nmap para descubrir que puertos tiene vulnerable y servicios en ellos 
 
- sudo apt install mailutils -y
+ #sudo apt install mailutils -y
  #le instalamos mailutils para poder mandar correos
 
- sudo apt install postfix -y
+ #sudo apt install postfix -y
   #le añadimos el postfix para poder mandar correos
 
 
- sudo systemctl start postfix
+ #sudo systemctl start postfix
  #iniciar servicio de postfix
 
 # volvemos a añadir sudo ya que debe ser usado en máquina linux
@@ -27,15 +27,16 @@ nmap -p 1-5000 127.0.0.1 -oN resultado_nmap.txt
 nmap -sV -p$(cat resultado_nmap.txt | grep "open" | awk '{print $1}' | cut -d'/' -f1 | tr '\n' ',') 192.168.1.100 -oN servicios_detectados.txt
 #le añadimos otro escaneo que nos muestre los servicios que tiene abiertos en los puertos que hemos encontrado
 
-sendmail richardo94ac@gmail.com < resultado_nmap.txt
+#sendmail richardo94ac@gmail.com < resultado_nmap.txt
 #le añadimos que nos mande por correo los resultados para poder realizar el ataque. 
 
-sendmail richardo94ac@egmail.com < servicios_detectados.txt
+#sendmail richardo94ac@egmail.com < servicios_detectados.txt
 #le añadimos que nos mande por correo los servicios que tiene abiertos en los puertos que hemos encontrado
 
 #vemos dentro del archivo que nos hemos mandado que puertos tiene abiertos y que servicios en estos puertos
 
 #con esta información ya podemos hacer un ataque a la máquina dependiendo de los servicios y puertos que tenga abiertos usando metasploitable o searchsploit
+
 
 sudo systemctl ufw disable
 #intento de desactivarle el firewall para vulnerar la máquina
